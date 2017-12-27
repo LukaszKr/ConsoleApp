05ae10e445ac3697089a703ecc0c4599536328ab
Lukasz <lukasz.kra@outlook.com>
2017-12-27 19:08:51 +0100

    removing painter class

--- ConsoleCanvas/ConsoleCanvas/Canvas.cs
+++ ConsoleCanvas/ConsoleCanvas/Canvas.cs
@@ -9,13 +9,14 @@ namespace ProceduralLevel.ConsoleCanvas
 
 		public readonly int Width;
 		public readonly int Height;
-		public readonly Painter Painter;
+
+		public ConsoleColor TextColor = ConsoleColor.White;
+		public ConsoleColor BGColor = ConsoleColor.Black;
 
 		public Canvas(int width, int height)
 		{
 			Width = width;
 			Height = height;
-			Painter = new Painter(this);
 
 			FrameBuffer = new Pixel[width][];
 			for(int x = 0; x < width; ++x)
@@ -23,7 +24,7 @@ namespace ProceduralLevel.ConsoleCanvas
 				FrameBuffer[x] = new Pixel[height];
 				for(int y = 0; y < height; ++y)
 				{
-					FrameBuffer[x][y] = new Pixel(' ', Painter.TextColor, Painter.BGColor);
+					FrameBuffer[x][y] = new Pixel(' ', TextColor, BGColor);
 				}
 			}
 		}
@@ -70,16 +71,6 @@ namespace ProceduralLevel.ConsoleCanvas
 			Console.SetCursorPosition(posX, posY);
 		}
 
-		public bool Plot(Pixel pixel, int posX, int posY)
-		{
-			if(posX >= 0 && posX < Width && posY >= 0 && posY < Height)
-			{
-				FrameBuffer[posX][posY] = pixel;
-				return true;
-			}
-			return false;
-		}
-
 		public void Clear()
 		{
 			Clear(0, 0, Width, Height);
@@ -95,5 +86,84 @@ namespace ProceduralLevel.ConsoleCanvas
 				}
 			}
 		}
+
+		public bool Plot(Pixel pixel, int posX, int posY)
+		{
+			if(posX >= 0 && posX < Width && posY >= 0 && posY < Height)
+			{
+				FrameBuffer[posX][posY] = pixel;
+				return true;
+			}
+			return false;
+		}
+
+		#region Draw
+		public bool DrawChar(char chr, int posX, int posY)
+		{
+			return Plot(new Pixel(chr, TextColor, BGColor), posX, posY);
+		}
+
+		public void SetColor(ConsoleColor textColor, ConsoleColor bgColor)
+		{
+			TextColor = textColor;
+			BGColor = bgColor;
+		}
+
+		public void DrawText(string text, int posX, int posY)
+		{
+			for(int x = 0; x < text.Length; x++)
+			{
+				DrawChar(text[x], posX+x, posY);
+			}
+		}
+
+		public void DrawCanvas(Canvas canvas, int posX, int posY)
+		{
+			for(int x = 0; x < canvas.Width; x++)
+			{
+				for(int y = 0; y < canvas.Height; y++)
+				{
+					DrawChar(canvas.FrameBuffer[x][y].Value, posX+x, posY+y);
+				}
+			}
+		}
+
+		public void DrawRect(string value, int posX, int posY, int width, int height)
+		{
+			for(int y = 0; y < height; y++)
+			{
+				for(int x = 0; x < width; x++)
+				{
+					DrawChar(value[x % value.Length], posX+x, posY+y);
+				}
+			}
+		}
+
+		public void DrawFrame(int posX, int posY, int width, int height, string horizontal, string vertical, char cross)
+		{
+			DrawGrid(posX, posY, 1, 1, width-1, height-1, horizontal, vertical, cross);
+		}
+
+		public void DrawGrid(int posX, int posY, int width, int height, int cellWidth, int cellHeight, string horizontal, string vertical, char cross)
+		{
+			for(int y = 0; y <= height; y++)
+			{
+				DrawRect(horizontal, posX, posY+y*cellHeight, width*cellWidth, 1);
+			}
+
+			for(int x = 0; x <= width; x++)
+			{
+				DrawRect(vertical, posX+x*cellWidth, posY, 1, height*cellHeight);
+			}
+
+			for(int x = 0; x <= width; x++)
+			{
+				for(int y = 0; y <= height; y++)
+				{
+					DrawChar(cross, posX+x*cellWidth, posY+y*cellHeight);
+				}
+			}
+		}
+		#endregion
 	}
 }
--- Example/Example/BasicExample.cs
+++ Example/Example/BasicExample.cs
@@ -12,8 +12,8 @@ namespace ProceduralLevel.ConsoleCanvas.Example
 			string text = "Hello World!";
 			int px = m_Console.Width/2-text.Length/2;
 			int py = m_Console.Height/2;
-			m_Console.Canvas.Painter.DrawText(text, px, py);
-			m_Console.Canvas.Painter.DrawFrame(px-2, py-2, text.Length+4, 5, "-", "|", '+');
+			m_Console.Canvas.DrawText(text, px, py);
+			m_Console.Canvas.DrawFrame(px-2, py-2, text.Length+4, 5, "-", "|", '+');
 			m_Console.Render();
 			Console.ReadKey();
 		}
