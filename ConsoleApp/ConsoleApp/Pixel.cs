acb334daab4909d6191d5683f49822a2459db784
Lukasz <lukasz.kra@outlook.com>
2017-12-30 11:41:31 +0100

    move file

---
+++ ConsoleApp/ConsoleApp/Pixel.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ProceduralLevel.ConsoleApp
+{
+	[StructLayout(LayoutKind.Explicit)]
+	public struct Pixel
+	{
+		[FieldOffset(0)] public readonly char Value;
+		[FieldOffset(2)] public readonly ushort Attributes;
+
+		public Pixel(char value, EColor textColor, EColor bgColor)
+		{
+			Value = value;
+			Attributes = (ushort)((ushort)textColor | ((ushort)bgColor << 4));
+		}
+
+		public override string ToString()
+		{
+			return string.Format("['{0}', Attributes: {1}]", Value.ToString(), Attributes.ToString());
+		}
+	}
+}
